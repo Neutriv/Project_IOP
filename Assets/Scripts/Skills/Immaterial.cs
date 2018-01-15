9ab7d7eef26af5f89cf63807d79acecd52c0a026
Galados191 <galados191@gmail.com>
2018-01-15 12:04:56 +0100

    Skills
    skończone

---
+++ Assets/Scripts/Skills/Immaterial.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Immaterial : MonoBehaviour {
+
+    public Player player;
+    public float timeStamp = 0f;
+    public float timeStamp2 = 0f;
+    public float coolDown = 2f;
+    public bool used = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject Obiekt = GameObject.Find("Player");
+        Player player = Obiekt.GetComponent<Player>();
+        if (player.Immaterial_lvl1)
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                if (timeStamp + coolDown + 4f <= Time.time)
+                {
+                    player.speed = 0.07f;
+                    player.shooting_off = true;
+                    gameObject.GetComponent<BoxCollider>().isTrigger = true;
+                    used = true;
+                    timeStamp = Time.time;
+
+                }
+            }
+            if (used)
+            {
+                if (timeStamp + coolDown <= Time.time)
+                {
+                    player.speed = 0.1f;
+                    gameObject.GetComponent<BoxCollider>().isTrigger = false;
+                    used = false;
+                    player.shooting_off = false;
+                }
+            }
+        }
+
+    }
+
+
+    }
